95b7e3f2afa6235271851afbc12130d7ec56242a
Ketbome <pablo.moraga@mail.udp.cl>
2024-02-20 10:32:53 -0300

    Añadir configuración de salida y funciones de obtención de lanzamientos

---
+++ src/services/spacex.ts
@@ -0,0 +1,27 @@
+import { type APIxLaunches, type Doc } from "../types/api";
+
+export const getLaunchById = async (id: string) => {
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`);
+
+  const launch = (await res.json()) as Doc;
+  return launch;
+};
+
+export const getLaunches = async () => {
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        date_unix: "asc",
+      },
+      limit: 12,
+    }),
+  });
+
+  const { docs: launches } = (await res.json()) as APIxLaunches;
+  return launches;
+};
